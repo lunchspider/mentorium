b63b1ac024cd97c16efc50826c288800f1f45efb
anushree <aushreebondia@gmail.com>
2024-02-03 19:37:37 +0530

    signin

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Projects } from "@/components/Projects";
+import Page from "@/app/sign-in/page";
 
 export default function Home() {
   return (
     <div className="flex justify-center items-center h-screen">
-      <Projects />
+      <Page />
+      {/* <Projects /> */}
     </div>
   );
 }
