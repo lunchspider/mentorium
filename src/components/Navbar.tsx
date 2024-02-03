9d61c57f0d58ec1cdd89290aaabbff0727af375d
ayuugoyal <ayushgoyal8178@gmail.com>
2024-02-03 13:08:59 +0530

    feat: add dark mode

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,35 @@
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
+import { ModeToggle } from "./Darkmode";
+
+const Navbar = () => {
+  return (
+    <Menubar>
+      <MenubarMenu>
+        <div className="flex justify-between w-full px-28">
+          <MenubarTrigger>File</MenubarTrigger>
+          <MenubarContent>
+            <MenubarItem>
+              New Tab <MenubarShortcut>⌘T</MenubarShortcut>
+            </MenubarItem>
+            <MenubarItem>New Window</MenubarItem>
+            <MenubarSeparator />
+            <MenubarItem>Share</MenubarItem>
+            <MenubarSeparator />
+            <MenubarItem>Print</MenubarItem>
+          </MenubarContent>
+          <ModeToggle />
+        </div>
+      </MenubarMenu>
+    </Menubar>
+  );
+};
+
+export default Navbar;
